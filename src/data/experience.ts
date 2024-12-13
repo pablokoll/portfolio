d93a8f54a9a36053f84ee383ac14753b2143c87b
ideapad-w11-wsl <pablokollm@gmail.com>
2024-12-13 20:52:15 +0100

    updated cv dec 13 2024

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -2,19 +2,45 @@ import type { Experience } from '../types/types';
 
 export const experience: Experience[] = [
 	{
+		title: 'Full Stack Developer',
+		company: 'Tendam',
+		company_link: 'https://www.linkedin.com/company/tendam/',
+		date_start: 'Dec 2024',
+		date_end: 'Present',
+		description: [
+			'I work with Salesforce Commerce B2C with cartridges and pipelines, and I have experience with Salesforce Commerce Cloud (SFCC) and Demandware.',
+			'Working with methodologies such as Agile and Scrum to deliver projects on time.',
+			'Utilizing Jira and Confluence for project management and documentation.',
+		],
+	},
+	{
+		title: 'Full Stack Developer',
+		company: 'Webmefy',
+		company_link: 'https://www.linkedin.com/company/webmefy/',
+		date_start: 'Jul 2024',
+		date_end: 'Nov 2024',
+		description: [
+			'Full Stack development, implementing changes in Shopify frontends using various themes based on Liquid, HTML, CSS, and JavaScript',
+			'Integrating different systems to add new functionalities to eCommerce platforms, utilizing Shopify’s GraphQL, REST, and StoreFront APIs',
+			'Implementing client services such as ERPs, CRMs, or various web services to enhance functionality',
+			'Utilizing Amazon Web Services for hosting and deploying backend applications',
+			'Working closely with clients, maintaining and delivering new projects',
+			'Applying Agile and Waterfall methodologies, tracking tickets and timelines for efficient project management',
+		],
+	},
+	{
 		title: 'Backend Developer',
 		company: 'Balloon Group',
 		company_link: 'https://www.linkedin.com/company/balloon-group/',
 		date_start: 'Jan 2023',
-		date_end: 'Present',
+		date_end: 'Jul 2024',
 		description: [
-			'My technology stack and development environment are Node.js (Typescript/Javascript), Docker, and Ubuntu (Linux). My primary frameworks are Express.js and Nest.js.',
-			'I led the backend development across various projects, focusing on understanding daily client needs.',
-			`I'm a Backend Developer specializing in microservices for E-Commerce, I bring expertise from working with platforms like Magento and Vtex.`,
-			'I adeptly handle REST, SOAP, and GraphQL APIs, along with protocols such as HTTPS, SFTP, and SMTP to facilitate seamless communication across diverse software systems.',
-			'For secure access management, I employed methods like OAuth 2.0 and JWT.',
-			'To ensure application quality and reliability, I utilize both end-to-end (E2E) and unit testing with tools including Jest and Mocha.',
-			'My deployment process is streamlined through CI/CD practices, and I hosted applications efficiently on cloud services like AWS.',
+			'I work with a technology stack that includes Node.js (TypeScript/JavaScript), Docker, and Ubuntu (Linux). My primary frameworks are Express.js and Nest.js.',
+			'As a Backend Developer, my role focused on specializing in microservices architecture, particularly for E-Commerce platforms like Magento and Vtex.',
+			'Self‑managing projects from a technical perspective, understanding client needs, and ensuring development quality.',
+			'For seamless communication between systems, I utilize protocols such as HTTPS and SFTP, and handle APIs including REST, SOAP, and GraphQL. I apply authentication methods like OAuth2, JWT, API Keys, and Token-Based Authentication.',
+			'To guarantee the quality and reliability of applications, I employ a combination of end-to-end (E2E) and unit testing methodologies',
+			'I work with CI/CD practices and collaborate closely with the DevOps team to efficiently host applications on cloud services like AWS. Additionally, I utilize Git for effective team collaboration and robust version control.',
 		],
 	},
 	{
